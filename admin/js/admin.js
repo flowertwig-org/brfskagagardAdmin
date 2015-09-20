23903a371e792f160388fd876e24f0cf403225e5
Mattias Flowertwig <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2015-09-20 16:27:14 +0200

    admin - we can now change mood image

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -292,8 +292,7 @@
 
                             var parentNode = node.parentNode;
                             var tmpNode = node.nextElementSibling;
-                            if (tmpNode)
-                            {
+                            if (tmpNode) {
                                 node = tmpNode;
                             } else {
                                 node = null;
@@ -369,8 +368,10 @@
             });
         });
 
+
         var moods = document.getElementsByClassName('mood');
         if (moods.length == 1) {
+            var mood = moods[0];
             var changeMood = document.createElement('span');
             changeMood.innerHTML = '<a href="#">Change image</a>';
 
@@ -381,25 +382,74 @@
             changeMood.style.backgroundColor = '#fff';
             changeMood.style.border = 'solid 1px lightgrey';
 
-            moods[0].appendChild(changeMood);
+            mood.appendChild(changeMood);
 
             changeMood.addEventListener('click', function (event) {
                 event.preventDefault();
-                this.innerHTML = "Loading images ...";
-                var container = this;
 
-                storage.list('/img/mood/', function (info, status) {
-                    console.log('arg', arguments);
-                    if (status.isOK) {
-                        var list = arguments[0];
-                        var elements = [];
-                        for (var i = 0; i < list.length; i++) {
-                            list[i]
-                            elements.push('<img src="' + list[i].path + '" width="200" />');
+                if (event.target.innerHTML.indexOf('Change image') >= 0) {
+                    this.innerHTML = "Loading images ...";
+                    var container = this;
+
+                    storage.list('/img/mood/', function (info, status) {
+                        console.log('arg', arguments);
+                        if (status.isOK) {
+                            var list = arguments[0];
+                            var elements = [];
+                            for (var i = 0; i < list.length; i++) {
+                                list[i]
+                                elements.push('<img src="' + list[i].path + '" width="200" title="' + list[i].name + '" style="cursor:pointer" />');
+                            }
+                            container.innerHTML = elements.join('');
                         }
-                        container.innerHTML = elements.join('');
+                    });
+
+                } else if (event.target.innerHTML.indexOf('Save') >= 0) {
+                    // TODO: store mood image changes
+                    var resourceName = location.pathname.substring(1);
+                    storage.get(resourceName, function (fileMeta, callStatus) {
+                        if (callStatus.isOK) {
+                            var newData = fileMeta.data;
+
+                            var image = mood.style.backgroundImage;
+                            var index = image.indexOf('/img/mood');
+                            image = '.' + image.substring(index);
+                            image = image.substring(0, image.length - 1);
+
+                            newData = newData.replace(mood.getAttribute('data-mood-orginal'), image);
+
+                            storage.set(resourceName, newData, function (fileMeta2, callStatus2) {
+                                if (callStatus2.isOK) {
+                                    alert('saved');
+                                } else {
+                                    alert('fail, error code: 1');
+                                }
+                            });
+                        } else {
+                            alert('fail, error code: 0');
+                        }
+                    });
+                } else {
+                    if (event.target.tagName.toLowerCase() == 'img') {
+                        var index = 0;
+                        if (!mood.getAttribute('data-mood-orginal')) {
+                            var orgImage = mood.style.backgroundImage;
+                            index = orgImage.indexOf('/img/mood');
+                            orgImage = orgImage.substring(index);
+                            orgImage = orgImage.substring(0, orgImage.length -1);
+                            mood.setAttribute('data-mood-orginal', '.' + orgImage);
+                        }
+
+                        var image = event.target.src;
+                        index = image.indexOf('/img/mood');
+                        image = image.substring(index);
+                        orgImage = image.substring(0, image.length - 1);
+                        mood.style.backgroundImage = 'url(.' + image + ')';
+                        changeMood.innerHTML = '<a href="#">Change image</a> | <a href="#">Save</a>';
+                    } else {
+                        changeMood.innerHTML = '<a href="#">Change image</a>';
                     }
-                });
+                }
             });
         }
     }
