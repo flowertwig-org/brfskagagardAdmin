4cdc8aa9eda243c0ad0ac0c3f1026d9c4f3c087b
Mattias Flowertwig <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2015-09-20 16:56:53 +0200

    Admin - allow change of more pages

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -72,6 +72,156 @@
         }, 100);
     }
 
+
+    function changeMood(storage) {
+        var moods = document.getElementsByClassName('mood');
+        if (moods.length == 1) {
+            var mood = moods[0];
+            var changeMood = document.createElement('span');
+            changeMood.innerHTML = '<a href="#">Change image</a>';
+
+            changeMood.style.position = 'absolute';
+            changeMood.style.zIndex = 10;
+            changeMood.style.margin = '20px';
+            changeMood.style.padding = '10px';
+            changeMood.style.backgroundColor = '#fff';
+            changeMood.style.border = 'solid 1px lightgrey';
+
+            mood.appendChild(changeMood);
+
+            changeMood.addEventListener('click', function (event) {
+                event.preventDefault();
+
+                if (event.target.innerHTML.indexOf('Change image') >= 0) {
+                    this.innerHTML = "Loading images ...";
+                    var container = this;
+
+                    storage.list('/img/mood/', function (info, status) {
+                        console.log('arg', arguments);
+                        if (status.isOK) {
+                            var list = arguments[0];
+                            var elements = [];
+                            for (var i = 0; i < list.length; i++) {
+                                list[i]
+                                elements.push('<img src="' + list[i].path + '" width="200" title="' + list[i].name + '" style="cursor:pointer" />');
+                            }
+                            container.innerHTML = elements.join('');
+                        }
+                    });
+
+                } else if (event.target.innerHTML.indexOf('Save') >= 0) {
+                    // TODO: store mood image changes
+                    var resourceName = location.pathname.substring(1);
+                    storage.get(resourceName, function (fileMeta, callStatus) {
+                        if (callStatus.isOK) {
+                            var newData = fileMeta.data;
+
+                            var image = mood.style.backgroundImage;
+                            var index = image.indexOf('/img/mood');
+                            image = '.' + image.substring(index);
+                            image = image.substring(0, image.length - 1);
+
+                            newData = newData.replace(mood.getAttribute('data-mood-orginal'), image);
+
+                            storage.set(resourceName, newData, function (fileMeta2, callStatus2) {
+                                if (callStatus2.isOK) {
+                                    alert('saved');
+                                } else {
+                                    alert('fail, error code: 1');
+                                }
+                            });
+                        } else {
+                            alert('fail, error code: 0');
+                        }
+                    });
+                } else {
+                    if (event.target.tagName.toLowerCase() == 'img') {
+                        var index = 0;
+                        if (!mood.getAttribute('data-mood-orginal')) {
+                            var orgImage = mood.style.backgroundImage;
+                            index = orgImage.indexOf('/img/mood');
+                            orgImage = orgImage.substring(index);
+                            orgImage = orgImage.substring(0, orgImage.length - 1);
+                            mood.setAttribute('data-mood-orginal', '.' + orgImage);
+                        }
+
+                        var image = event.target.src;
+                        index = image.indexOf('/img/mood');
+                        image = image.substring(index);
+                        orgImage = image.substring(0, image.length - 1);
+                        mood.style.backgroundImage = 'url(.' + image + ')';
+                        changeMood.innerHTML = '<a href="#">Change image</a> | <a href="#">Save</a>';
+                    } else {
+                        changeMood.innerHTML = '<a href="#">Change image</a>';
+                    }
+                }
+            });
+        }
+    }
+
+    function changeTextContent(storage) {
+        includeScript("//tinymce.cachefly.net/4.2/tinymce.min.js");
+        ensureLoaded('tinymce', window, function () {
+            tinymce.init({
+                selector: ".sw-editable",
+                inline: true,
+                menubar: false,
+                browser_spellcheck: true,
+                plugins: "save",
+                toolbar: "save | news-item-above news-item-below styleselect | bold italic | bullist numlist outdent indent | link image | undo redo",
+                save_onsavecallback: storeDefaultPage,
+                setup: function (editor) {
+                    // Add a custom button
+                    editor.addButton('news-item-above', {
+                        title: 'Add section above this section',
+                        image: './admin/img/newsItemAbove.png',
+                        onclick: function () {
+                            // Add you own code to execute something on click
+                            editor.focus();
+                            var node = editor.selection.getNode();
+                            while (node.tagName.toLowerCase() != 'div' && node.className != 'news-item') {
+                                node = node.parentNode;
+                            }
+
+                            var newsItem = document.createElement("div");
+                            newsItem.className = "news-item";
+                            newsItem.innerHTML = "<h2>Default title</h2><p>Default paragraf</p>";
+                            node.parentNode.insertBefore(newsItem, node);
+                        }
+                    });
+                    // Add a custom button
+                    editor.addButton('news-item-below', {
+                        title: 'Add section below this section',
+                        image: './admin/img/newsItemBelow.png',
+                        onclick: function () {
+                            // Add you own code to execute something on click
+                            editor.focus();
+                            var node = editor.selection.getNode();
+                            while (node.tagName.toLowerCase() != 'div' && node.className != 'news-item') {
+                                node = node.parentNode;
+                            }
+
+                            var newsItem = document.createElement("div");
+                            newsItem.className = "news-item";
+                            newsItem.innerHTML = "<h2>Default title</h2><p>Default paragraf</p>";
+
+                            var parentNode = node.parentNode;
+                            var tmpNode = node.nextElementSibling;
+                            if (tmpNode) {
+                                node = tmpNode;
+                            } else {
+                                node = null;
+                            }
+
+                            parentNode.insertBefore(newsItem, node);
+                        }
+                    });
+
+                }
+            });
+        });
+    }
+
     function updateParking(parking, isFree) {
         if (isFree) {
             parking.className = 'parking occupied';
@@ -112,7 +262,7 @@
         });
     }
 
-    function parkingPage() {
+    function parkingPage(storage) {
         var parkings = document.getElementsByClassName('parking');
         for (var i = 0; i < parkings.length; i++) {
             var parking = parkings[i];
@@ -124,6 +274,8 @@
                 updateParking(this, !isFree);
             });
         }
+
+        changeTextContent(storage);
     }
     function storeDefaultPage(editor) {
         if (editor && editor.startContent) {
@@ -304,154 +456,13 @@
                 }
             });
         });
+
+        changeMood(storage);
     }
 
     function defaultPage(storage) {
-        includeScript("//tinymce.cachefly.net/4.2/tinymce.min.js");
-        ensureLoaded('tinymce', window, function () {
-            tinymce.init({
-                selector: ".sw-editable",
-                inline: true,
-                menubar: false,
-                browser_spellcheck: true,
-                plugins: "save",
-                toolbar: "save | news-item-above news-item-below styleselect | bold italic | bullist numlist outdent indent | link image | undo redo",
-                save_onsavecallback: storeDefaultPage,
-                setup: function (editor) {
-                    // Add a custom button
-                    editor.addButton('news-item-above', {
-                        title: 'Add section above this section',
-                        image: './admin/img/newsItemAbove.png',
-                        onclick: function () {
-                            // Add you own code to execute something on click
-                            editor.focus();
-                            var node = editor.selection.getNode();
-                            while (node.tagName.toLowerCase() != 'div' && node.className != 'news-item') {
-                                node = node.parentNode;
-                            }
-
-                            var newsItem = document.createElement("div");
-                            newsItem.className = "news-item";
-                            newsItem.innerHTML = "<h2>Default title</h2><p>Default paragraf</p>";
-                            node.parentNode.insertBefore(newsItem, node);
-                        }
-                    });
-                    // Add a custom button
-                    editor.addButton('news-item-below', {
-                        title: 'Add section below this section',
-                        image: './admin/img/newsItemBelow.png',
-                        onclick: function () {
-                            // Add you own code to execute something on click
-                            editor.focus();
-                            var node = editor.selection.getNode();
-                            while (node.tagName.toLowerCase() != 'div' && node.className != 'news-item') {
-                                node = node.parentNode;
-                            }
-
-                            var newsItem = document.createElement("div");
-                            newsItem.className = "news-item";
-                            newsItem.innerHTML = "<h2>Default title</h2><p>Default paragraf</p>";
-
-                            var parentNode = node.parentNode;
-                            var tmpNode = node.nextElementSibling;
-                            if (tmpNode) {
-                                node = tmpNode;
-                            } else {
-                                node = null;
-                            }
-
-                            parentNode.insertBefore(newsItem, node);
-                        }
-                    });
-
-                }
-            });
-        });
-
-
-        var moods = document.getElementsByClassName('mood');
-        if (moods.length == 1) {
-            var mood = moods[0];
-            var changeMood = document.createElement('span');
-            changeMood.innerHTML = '<a href="#">Change image</a>';
-
-            changeMood.style.position = 'absolute';
-            changeMood.style.zIndex = 10;
-            changeMood.style.margin = '20px';
-            changeMood.style.padding = '10px';
-            changeMood.style.backgroundColor = '#fff';
-            changeMood.style.border = 'solid 1px lightgrey';
-
-            mood.appendChild(changeMood);
-
-            changeMood.addEventListener('click', function (event) {
-                event.preventDefault();
-
-                if (event.target.innerHTML.indexOf('Change image') >= 0) {
-                    this.innerHTML = "Loading images ...";
-                    var container = this;
-
-                    storage.list('/img/mood/', function (info, status) {
-                        console.log('arg', arguments);
-                        if (status.isOK) {
-                            var list = arguments[0];
-                            var elements = [];
-                            for (var i = 0; i < list.length; i++) {
-                                list[i]
-                                elements.push('<img src="' + list[i].path + '" width="200" title="' + list[i].name + '" style="cursor:pointer" />');
-                            }
-                            container.innerHTML = elements.join('');
-                        }
-                    });
-
-                } else if (event.target.innerHTML.indexOf('Save') >= 0) {
-                    // TODO: store mood image changes
-                    var resourceName = location.pathname.substring(1);
-                    storage.get(resourceName, function (fileMeta, callStatus) {
-                        if (callStatus.isOK) {
-                            var newData = fileMeta.data;
-
-                            var image = mood.style.backgroundImage;
-                            var index = image.indexOf('/img/mood');
-                            image = '.' + image.substring(index);
-                            image = image.substring(0, image.length - 1);
-
-                            newData = newData.replace(mood.getAttribute('data-mood-orginal'), image);
-
-                            storage.set(resourceName, newData, function (fileMeta2, callStatus2) {
-                                if (callStatus2.isOK) {
-                                    alert('saved');
-                                } else {
-                                    alert('fail, error code: 1');
-                                }
-                            });
-                        } else {
-                            alert('fail, error code: 0');
-                        }
-                    });
-                } else {
-                    if (event.target.tagName.toLowerCase() == 'img') {
-                        var index = 0;
-                        if (!mood.getAttribute('data-mood-orginal')) {
-                            var orgImage = mood.style.backgroundImage;
-                            index = orgImage.indexOf('/img/mood');
-                            orgImage = orgImage.substring(index);
-                            orgImage = orgImage.substring(0, orgImage.length -1);
-                            mood.setAttribute('data-mood-orginal', '.' + orgImage);
-                        }
-
-                        var image = event.target.src;
-                        index = image.indexOf('/img/mood');
-                        image = image.substring(index);
-                        orgImage = image.substring(0, image.length - 1);
-                        mood.style.backgroundImage = 'url(.' + image + ')';
-                        changeMood.innerHTML = '<a href="#">Change image</a> | <a href="#">Save</a>';
-                    } else {
-                        changeMood.innerHTML = '<a href="#">Change image</a>';
-                    }
-                }
-            });
-        }
+        changeTextContent(storage);
+        changeMood(storage);
     }
 
     function loadAdminState(token) {
