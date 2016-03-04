23a959a4bc0b58ff4d230a22422a4adbe839e30d
Mattias Flowertwig <mattias@Mattiass-MacBook.local>
2016-03-04 19:20:06 +0100

    more work on the flower twigMoodImage component

--- admin/js/components/flowertwigmoodimage.js
+++ admin/js/components/flowertwigmoodimage.js
@@ -14,46 +14,19 @@
                 resourceName += 'index.html';
             }
 
-            // var container = editor.bodyElement;
-            var content = container.innerHTML;
-
-            var image = mood.style.backgroundImage;
+            var image = container.style.backgroundImage;
             var index = image.indexOf('/img/mood');
             image = '.' + image.substring(index);
             image = image.substring(0, image.length - 1);
-
+            container.removeAttribute('data-mood-orginal');
+            
+            var content = container.outerHTML;
+            content = content.replace(container.style.backgroundImage, "url('" + image + "')");
+            
             staticWeb.updatePage(container.id, container.tagName, resourceName, content);
-
-            storage.get(resourceName, function (fileMeta, callStatus) {
-                if (callStatus.isOK) {
-                    var newData = fileMeta.data;
-
-                    var image = mood.style.backgroundImage;
-                    var index = image.indexOf('/img/mood');
-                    image = '.' + image.substring(index);
-                    image = image.substring(0, image.length - 1);
-
-                    newData = newData.replace(mood.getAttribute('data-mood-orginal'), image);
-
-                    if (newData.indexOf('<meta name="generator" content="StaticWeb" />') == -1) {
-                        newData = newData.replace('</head>', '<meta name="generator" content="StaticWeb" /></head>');
-                    }
-
-
-                    storage.set(resourceName, newData, function (fileMeta2, callStatus2) {
-                        if (callStatus2.isOK) {
-                            alert('saved');
-                        } else {
-                            alert('fail, error code: 1');
-                        }
-                    });
-                } else {
-                    alert('fail, error code: 0');
-                }
-            });
-
         },
         listImages: function (container) {
+            container.style.display = 'inline-block';
             container.innerHTML = "Loading images ...";
 
             staticWeb.storage.list('/img/mood/', function (info, status) {
@@ -72,29 +45,21 @@
                 }
             });
         },
-        changeImage: function(container) {
-                    var index = 0;
-                    if (!mood.getAttribute('data-mood-orginal')) {
-                        var orgImage = mood.style.backgroundImage;
-                        index = orgImage.indexOf('/img/mood');
-                        orgImage = orgImage.substring(index);
-                        orgImage = orgImage.substring(0, orgImage.length - 1);
-                        mood.setAttribute('data-mood-orginal', '.' + orgImage);
-                    }
+        changeImage: function (event, container) {
+            var index = 0;
+            if (!container.getAttribute('data-mood-orginal')) {
+                var orgImage = container.style.backgroundImage;
+                index = orgImage.indexOf('/img/mood');
+                orgImage = orgImage.substring(index);
+                orgImage = orgImage.substring(0, orgImage.length - 1);
+                container.setAttribute('data-mood-orginal', '.' + orgImage);
+            }
 
-                    var image = event.target.src;
-                    index = image.indexOf('/img/mood');
-                    image = image.substring(index);
-                    orgImage = image.substring(0, image.length - 1);
-                    mood.style.backgroundImage = 'url(.' + image + ')';
-
-                    changeMood.style.backgroundColor = '#2F5575';
-                    changeMood.style.color = '#fff';
-                    changeMood.style.border = '0';
-                    changeMood.style.borderRadius = '6px';
-
-                    changeMood.innerHTML = '<a href="#" style="text-decoration:none;color:#fff">Change image</a> | <a href="#" style="text-decoration:none;color:#fff">Save</a>';
-            
+            var image = event.target.src;
+            index = image.indexOf('/img/mood');
+            image = image.substring(index);
+            orgImage = image.substring(0, image.length - 1);
+            container.style.backgroundImage = 'url(.' + image + ')';
         },
         init: function () {
             var self = this;
@@ -103,22 +68,67 @@
             for (var index = 0; index < elements.length; index++) {
                 var container = elements[index];
                 var mood = container.children[0];
-                var changeMood = document.createElement('span');
-                changeMood.innerHTML = '<a href="#" style="color:#fff;text-decoration:none">Change image</a>';
+                var panel = document.createElement('span');
+                panel.style.position = 'absolute';
+                panel.style.zIndex = '10';
+                panel.style.margin = '20px';
+                panel.style.paddingTop = '10px';
+                
+                var imageArea = document.createElement('span');
+                var changeBtn = document.createElement('a');
+                var saveBtn = document.createElement('a');
+                
+                imageArea.style.display = 'inline-block';
+                imageArea.style.borderRadius = '6px';
+                imageArea.style.padding = '10px';
+                imageArea.style.backgroundColor = '#fff';
+                imageArea.style.color = '#000';
+                imageArea.addEventListener('click', function (event) {
+                    event.preventDefault();
+                    self.changeImage(event, mood);
+                    imageArea.style.display = 'none';
+                    changeBtn.style.display = 'inline';
+                    saveBtn.style.display = 'inline';
+                });
 
-                changeMood.style.position = 'absolute';
-                changeMood.style.zIndex = '10';
-                changeMood.style.margin = '20px';
-                changeMood.style.padding = '10px';
-                //changeMood.style.backgroundColor = '#fff';
-                //changeMood.style.border = 'solid 1px lightgrey';
-                changeMood.style.backgroundColor = '#2F5575';
-                changeMood.style.color = '#fff';
-                changeMood.style.borderRadius = '6px';
+                changeBtn.href = '#';
+                changeBtn.text = 'Change image';
+                changeBtn.style.color = '#fff';
+                changeBtn.style.textDecoration = 'none';
+                changeBtn.style.backgroundColor = '#2F5575';
+                changeBtn.style.color = '#fff';
+                changeBtn.style.borderRadius = '6px';
+                changeBtn.style.padding = '10px';
+                changeBtn.style.marginRight = '15px';
+                changeBtn.addEventListener('click', function (event) {
+                    event.preventDefault();
+                    changeBtn.style.display = 'none';
+                    saveBtn.style.display = 'none';
+                    self.listImages(imageArea);
+                });
 
-                mood.appendChild(changeMood);
+                saveBtn.href = '#';
+                saveBtn.text = 'Save';
+                saveBtn.style.color = '#fff';
+                saveBtn.style.textDecoration = 'none';
+                saveBtn.style.backgroundColor = '#2F5575';
+                saveBtn.style.color = '#fff';
+                saveBtn.style.borderRadius = '6px';
+                saveBtn.style.padding = '10px';
+                saveBtn.style.display = 'none';
+                saveBtn.addEventListener('click', function (event) {
+                    event.preventDefault();
+                    mood.removeChild(panel);
+                    self.save(mood);
+                    mood.appendChild(panel);
+                });
+                
+                panel.appendChild(changeBtn);
+                panel.appendChild(saveBtn);
+                panel.appendChild(imageArea);
 
-                this.setupAction(mood, changeMood);
+                mood.appendChild(panel);
+                // this.setupAction(panel, changeBtn);
             }
         },
         setupAction: function (container, changeMoodBtn) {
@@ -134,7 +144,12 @@
                     if (event.target.tagName.toLowerCase() == 'img') {
                         self.changeImage(this);
                     } else {
+                        changeMood.style.backgroundColor = '#2F5575';
+                        changeMood.style.color = '#fff';
+                        changeMood.style.border = '0';
+                        changeMood.style.borderRadius = '6px';
 
+                        changeMood.innerHTML = '<a href="#" style="text-decoration:none;color:#fff">Change image</a>';
                     }
                 }
             });
@@ -221,34 +236,34 @@ function changeMood(storage) {
                 //                 });
             } else {
                 if (event.target.tagName.toLowerCase() == 'img') {
-//                     var index = 0;
-//                     if (!mood.getAttribute('data-mood-orginal')) {
-//                         var orgImage = mood.style.backgroundImage;
-//                         index = orgImage.indexOf('/img/mood');
-//                         orgImage = orgImage.substring(index);
-//                         orgImage = orgImage.substring(0, orgImage.length - 1);
-//                         mood.setAttribute('data-mood-orginal', '.' + orgImage);
-//                     }
-// 
-//                     var image = event.target.src;
-//                     index = image.indexOf('/img/mood');
-//                     image = image.substring(index);
-//                     orgImage = image.substring(0, image.length - 1);
-//                     mood.style.backgroundImage = 'url(.' + image + ')';
-// 
-//                     changeMood.style.backgroundColor = '#2F5575';
-//                     changeMood.style.color = '#fff';
-//                     changeMood.style.border = '0';
-//                     changeMood.style.borderRadius = '6px';
-// 
-//                     changeMood.innerHTML = '<a href="#" style="text-decoration:none;color:#fff">Change image</a> | <a href="#" style="text-decoration:none;color:#fff">Save</a>';
+                    //                     var index = 0;
+                    //                     if (!mood.getAttribute('data-mood-orginal')) {
+                    //                         var orgImage = mood.style.backgroundImage;
+                    //                         index = orgImage.indexOf('/img/mood');
+                    //                         orgImage = orgImage.substring(index);
+                    //                         orgImage = orgImage.substring(0, orgImage.length - 1);
+                    //                         mood.setAttribute('data-mood-orginal', '.' + orgImage);
+                    //                     }
+                    // 
+                    //                     var image = event.target.src;
+                    //                     index = image.indexOf('/img/mood');
+                    //                     image = image.substring(index);
+                    //                     orgImage = image.substring(0, image.length - 1);
+                    //                     mood.style.backgroundImage = 'url(.' + image + ')';
+                    // 
+                    //                     changeMood.style.backgroundColor = '#2F5575';
+                    //                     changeMood.style.color = '#fff';
+                    //                     changeMood.style.border = '0';
+                    //                     changeMood.style.borderRadius = '6px';
+                    // 
+                    //                     changeMood.innerHTML = '<a href="#" style="text-decoration:none;color:#fff">Change image</a> | <a href="#" style="text-decoration:none;color:#fff">Save</a>';
                 } else {
-                    changeMood.style.backgroundColor = '#2F5575';
-                    changeMood.style.color = '#fff';
-                    changeMood.style.border = '0';
-                    changeMood.style.borderRadius = '6px';
-
-                    changeMood.innerHTML = '<a href="#" style="text-decoration:none;color:#fff">Change image</a>';
+                    //                     changeMood.style.backgroundColor = '#2F5575';
+                    //                     changeMood.style.color = '#fff';
+                    //                     changeMood.style.border = '0';
+                    //                     changeMood.style.borderRadius = '6px';
+                    // 
+                    //                     changeMood.innerHTML = '<a href="#" style="text-decoration:none;color:#fff">Change image</a>';
                 }
             }
         });
