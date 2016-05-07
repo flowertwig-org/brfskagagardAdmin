838ad2e709ce2d005e98dfd4d9761a9aef1285fc
Mattias Flowertwig <mattias@Mattiass-MacBook.local>
2016-05-07 22:31:31 +0200

    added possibility to ignore paths

--- admin/js/swconfig.js
+++ admin/js/swconfig.js
@@ -4,12 +4,16 @@
         'type': 'github',
         'repo': 'flowertwig-org/brfskagagardAdmin'
     }
+    sw.config.user = {
+        'timeout': 60 * 60 * 24 * 7 // Time settings is stored on user device (currently: 7 days)
+    };
     sw.config.cookieName = 'staticweb-token';
     sw.config.onPage = {
         // Tells StaticWeb show general menu, options and more not present as components on page
         'display': 'onDemand', // 'onDemand', 'always', 'no' 
         'navigation': {
-            'display': 'onDemand' // 'onDemand', 'always', 'no'
+            'display': 'onDemand', // 'onDemand', 'always', 'no'
+            'ignorePaths': ['.gitignore', 'README.md', 'img', 'css', 'docs', 'admin', 'LICENSE', 'parking.html']
         }
     };
 })(StaticWeb);
