27549fce8a08c31730c80133642cffcb94ae930a
Mattias Flowertwig <mattias@Mattiass-MacBook.local>
2016-05-08 18:04:39 +0200

    navigation - we now have a working tree view

--- admin/js/swonpage-navigation.js
+++ admin/js/swonpage-navigation.js
@@ -1,6 +1,38 @@
 /* global StaticWeb */
 (function (sw) {
 
+    var navigation = [];
+    var locations = [];
+
+    function getLocations() {
+        var locationArray = location.pathname.split('/');
+        // remove empty items
+        var index = 0;
+        while (locationArray.length > index) {
+            if (locationArray[index] == "") {
+                locationArray.splice(index, 1);
+            }
+            else {
+                index++;
+            }
+        }
+
+        var locations = [];
+        while (locationArray.length) {
+            var url = '/' + locationArray.join('/');
+            if (url[url.length - 1] !== '/') {
+                url = url + '/';
+            }
+
+            locations.push(url);
+            locationArray.splice(locationArray.length - 1, 1);
+        }
+        locations.push('/');
+        locations.reverse();
+
+        return locations;
+    }
+
     function getPath(name) {
         var name = name.replace('/index.html', '');
         if (name.length > 0 && name[name.length - 1] !== '/') {
@@ -17,95 +49,92 @@
         if (name.length > 0 && name[name.length - 1] === '/') {
             name = name.substring(0, name.length - 1);
         }
+        
+        var tmp = name.split('/');
+        name = tmp[tmp.length - 1];
+        
         if (name === '') {
             name = '/';
         }
+        
         return name;
     }
 
-    function createItem(item, depth) {
-        if (!sw.inAdminPath() && sw.config.onPage.navigation.ignorePaths.indexOf(item.name) !== -1) {
+    function createNode(item, depth) {
+        if (!sw.inAdminPath() && sw.config.onPage.navigation.ignorePaths.indexOf(item.link) !== -1) {
             return '';
         }
-        
-        var displayName = getDisplayName(item.path);
-        var link = displayName;
-        
-        var path = getPath(item.path);
-        var locationPath = getPath(location.pathname);
 
-        var isSelected = (locationPath === path);
         var hasDepth = depth > 0;
-        
+
+        var displayName = item.name;
         while (depth) {
             displayName = "&nbsp;&nbsp;" + displayName;
             depth--;
         }
-        
-        if (isSelected && hasDepth) {
+
+        if (item.isSelected && hasDepth) {
             displayName = displayName.replace("&nbsp;", "");
         }
 
-        var file = '<li class="' + (isSelected ? 'sw-onpage-navigation-item-selected' : '') + '" title="' + item.name + '" data-sw-nav-item-path="' + item.path + '" data-sw-nav-item-folder="' + path + '" data-sw-nav-item-type="file"><span>';
-        file += '<a href="' + link + '" class="sw-onpage-navigation-item-link">' + displayName + '</a>';
-        if (isSelected) {
+        var file = '<li class="' + (item.isSelected ? 'sw-onpage-navigation-item-selected' : '') + '" title="' + item.name + '" data-sw-nav-item-path="' + item.path + '" data-sw-nav-item-folder="' + item.path + '" data-sw-nav-item-type="file"><span>';
+        file += '<a href="' + item.link + '" class="sw-onpage-navigation-item-link">' + displayName + '</a>';
+        if (item.isSelected) {
             file += '<a href="#" title="Delete ' + displayName + '" class="sw-onpage-navigation-item-delete">x</a>';
             file += '<a href="#" title="Add a sub-page for ' + displayName + '" class="sw-onpage-navigation-item-add">+</a>';
         }
         file += '</li>';
         var index = 0;
         while (item.children.length > index) {
-            file += createItem(item.children[index], depth + 1);
+            file += createNode(item.children[index], depth + 1);
             index++;
         }
 
         return file;
     }
-    
-    function sortItems(a, b) {
-            var aPath = a.test; // getPath(a.path);
-            var bPath = b.test; // getPath(b.path);
-            
-            if (aPath < bPath) {
-                return -1;
-            }
-            if (aPath > bPath) {
-                return 1;
-            }
-            return 0;
-    }
 
+    function sortItems(a, b) {
+        var aPath = a.path; // getPath(a.path);
+        var bPath = b.path; // getPath(b.path);
+
+        if (aPath < bPath) {
+            return -1;
+        }
+        if (aPath > bPath) {
+            return 1;
+        }
+        return 0;
+    }
+    
     function createNodeWithItems(list) {
         var node = document.createElement("ul");
         node.className = 'sw-onpage-navigation-items';
         
-        // update object
-        for (var index = 0; index < list.length; index++) {
-            var item = list[index];
-            item.test = getPath(item.path);
-            item.children = [];
-        }
-
+        // Sort items
         list.sort(sortItems);
         
-        var index = 1;
-        while (list.length > index && list[index]) {
-            var prev = list[index -1];
-            var current = list[index];
-            if (current.test.indexOf(prev.test) !== -1) {
-                prev.children.push(current);
+        // Remove duplicates
+        var index = 0;
+        var prevNodePath = false;
+        while (list.length > index) {
+            var tmpPath = list[index].path;
+            if (prevNodePath === tmpPath) {
                 list.splice(index, 1);
             }else {
                 index++;
             }
-        }
+            prevNodePath = tmpPath;
+        }        
         
         var files = '';
         var folders = '';
         var depth = 0;
+        var prevItemPath = false;
         for (var index = 0; index < list.length; index++) {
             var item = list[index];
-            files += createItem(item, depth);
+            if (prevItemPath !== item.path) {
+                files += createNode(item, item.depth);
+            }
         }
 
         node.innerHTML = folders + files;
@@ -164,42 +193,80 @@
         });
     }
 
-    function showNavigation(navigation, navigationHeader, navigationList) {
-        sw.storage.list('/', function (list, callStatus) {
+    function createItem(item) {
+        if (!sw.inAdminPath() && sw.config.onPage.navigation.ignorePaths.indexOf(item.name) !== -1) {
+            return false;
+        }
+
+        var displayName = getDisplayName(item.path);
+        var link = displayName;
+        var path = getPath(item.path);
+        var locationPath = getPath(location.pathname);
+        var isSelected = (locationPath === path);
+
+        return {
+            'name': displayName,
+            'link': item.path,
+            'path': path,
+            'children': [],
+            'depth': path.split('/').length -1,
+            'isSelected': isSelected
+        }
+    }
+
+    function getNavigationNodes(path, itemElement, headerElement, contentElement) {
+        sw.storage.list(path, function (list, callStatus) {
             if (callStatus.isOK) {
-                var node = createNodeWithItems(list);
-                navigationList.appendChild(node);
-
-                node.addEventListener('click', function (e) {
-                    if (e.target.classList.contains('sw-onpage-navigation-item-add')) {
-                        var addr = e.target.parentNode.parentNode.getAttribute('data-sw-nav-item-path');
-                        var folder = e.target.parentNode.parentNode.getAttribute('data-sw-nav-item-folder');
-
-                        showCreatePageDialog(addr, folder);
+                for (var i = 0; i < list.length; i++) {
+                    var item = createItem(list[i]);
+                    if (item) {
+                        navigation.push(item);
                     }
-                    else if (e.target.classList.contains('sw-onpage-navigation-item-delete')) {
-                        var addr = e.target.parentNode.parentNode.getAttribute('data-sw-nav-item-path');
-                        if (confirm('Are you sure you want to delete "' + addr + '"?')) {
-                            sw.storage.del(addr + '/index.html', function (status) {
-                                if (status.isOK) {
-                                    console.log('successfully deleted page', addr);
-                                }
-                            })
-                        }
-                    }
-                });
+                }
 
-                navigation.setAttribute('data-sw-nav-expandable', '1');
-                navigationHeader.style.paddingBottom = '5px';
-                navigationHeader.style.borderBottom = 'solid 3px rgb(47, 85, 117)';
+                showNavigation(navigation, itemElement, headerElement, contentElement);
             }
         });
+    }
+
+
+    function showNavigation(navigation, navigationNode, navigationHeaderNode, navigationListNode) {
+        var node = createNodeWithItems(navigation);
+        navigationListNode.innerHTML = '';
+        navigationListNode.appendChild(node);
+
+        node.addEventListener('click', function (e) {
+            if (e.target.classList.contains('sw-onpage-navigation-item-add')) {
+                var addr = e.target.parentNode.parentNode.getAttribute('data-sw-nav-item-path');
+                var folder = e.target.parentNode.parentNode.getAttribute('data-sw-nav-item-folder');
+
+                showCreatePageDialog(addr, folder);
+            }
+            else if (e.target.classList.contains('sw-onpage-navigation-item-delete')) {
+                var addr = e.target.parentNode.parentNode.getAttribute('data-sw-nav-item-path');
+                if (confirm('Are you sure you want to delete "' + addr + '"?')) {
+                    sw.storage.del(addr + '/index.html', function (status) {
+                        if (status.isOK) {
+                            console.log('successfully deleted page', addr);
+                        }
+                    })
+                }
+            }
+        });
+
+        navigationNode.setAttribute('data-sw-nav-expandable', '1');
+        navigationHeaderNode.style.paddingBottom = '5px';
+        navigationHeaderNode.style.borderBottom = 'solid 3px rgb(47, 85, 117)';
     }
 
     var itemElement = document.getElementsByClassName('sw-onpage-navigation-item')[0];
     var headerElement = itemElement.getElementsByClassName('sw-onpage-options-item-header')[0];
     var contentElement = itemElement.getElementsByClassName('sw-onpage-options-item-content')[0];
 
-    showNavigation(itemElement, headerElement, contentElement);
 
+    locations = getLocations();
+    for (var i = 0; i < locations.length; i++) {
+        getNavigationNodes(locations[i], itemElement, headerElement, contentElement);
+    }
+    
 })(StaticWeb);
